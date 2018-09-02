a2ab81b1b448e7ecfda910871708f66c31a39d1c
Jaskaran singh (c017331) <36584015+C017331@users.noreply.github.com>
2018-09-02 17:26:54 +0530

    Update and rename README.md to kbc.c

---
+++ kbc.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+ int i,j,k;
+ 
+ printf("lets play kbc with your host linux");
+ printf("\n practise ques");
+ printf("\nwithout any further delay lets start");
+ 
+ for(i=0;i<=20;i++)
+  { 
+    
+   if(j==2||k==1)
+     {
+      printf("\nwell comeback after proper preprations");
+      break;
+     }
+  
+   switch(i)
+      {
+   
+        case 0 :
+          printf("\nQ.1: who is the father of c language?\n");
+          printf("1.charles babbage\t\t\t2.dennis ritchie\nans:");
+          break;
+        case 1 :
+          scanf("%d",&k);
+          break;
+        case 2 :
+          printf("congratulations on your right answer\nlets move to second question\n\n");
+          printf("\nQ.2: who was the first host of kbc?\n");
+          printf("1.amitabh bachhan\t\t\t2.shah rukh khan\nans:");
+          break;
+        case 3 :
+          scanf("%d",&j)       ;   
+          break;
+        case 4 :
+          printf("well done \nnext question\n\n");
+          printf("\nQ.3: in what language was python language written?\n");
+          printf("1.c language\t\t\t2.c++\nans:");
+          break;
+        case 5 :
+          scanf("%d",&j)      ;
+          break;     
+        case 6 :
+          printf("good job \n to the next one then\n\n");
+          printf("\nQ.4: who is the current prime minister of india?\n"); 
+          printf("1. narendra modi\t\t\t2. dr.manmohan singh\nans:");
+          break;
+        case 7 :
+          scanf("%d",&j)     ;        
+          break;
+        case 8:
+          printf("well you are qiute intelligent\n\n");
+          printf("\nQ.5: who has the third lagest army in the world?\n");
+          printf("1. canada\t\t\t2. india\nans:");
+          break;
+        case 9 :
+          scanf("%d",&k)    ;         
+          break;
+        case 10 :
+          printf("great!!\n next question\n\n");
+          printf("\nQ.6: which is the most stable form of carbon element?\n");
+          printf("1.graphite\t\t\t2.diamond\nans:");
+          break;
+        case 11 :
+          scanf("%d",&j)   ;            
+          break;
+        case 12 :
+          printf("you are doing great\n\n\n");
+          printf("\nQ.7: who is the author of national anthem of india?\n")  ;
+          printf("1.harivansh rai bachhan\t\t\t2.rabindra nath tagore\nans:");
+          break;
+        case 13 :
+          scanf("%d",&k)  ;             
+          break;
+        case 14 :
+          printf("well the following questions will not be so easy\n\n");
+          printf("\nQ.8: what is the value of number pi upto correct upto six digit?\n")  ;
+          printf("1.3.1415926\t\t\t2.3.1415823\nans:");         
+          break;
+        case 15 :
+          scanf("%d",&j);           
+          break;
+        case 16 :
+          printf("good!!second last question then\n\n");
+          printf("\nQ.9: who was the author of steins,gate?\n")  ;
+          printf("1.tomoya asano\t\t\t2.naotaka hayashi\nans:");
+          break;
+        case 17 :
+          scanf("%d",&k) ;          
+          break;
+        case 18 :
+          printf("you answered all questions right\nthe last question of program....\n\n");
+          printf("\nQ.10: which first number in natural numbers?\n");
+          printf("1.2         2.1\nans:");
+          break;
+         case 19:
+          scanf("%d",&k);
+          break;
+        default :
+          printf("\ncongratulations!!!!!!!!!!!!!!!!!! you have successfuly cracked all questions from the program\n\n\nstill we cant give any prize but only a felling of satisfaction");
+     }  ;
+     
+      
+
+  };
+
+ 
+
+}
+ 
+
